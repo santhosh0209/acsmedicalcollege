7500c71406b26b3f3a22e95952e09c033264beec
santhoshrocky0902 <santhoshrocky11@gmail.com>
2021-02-19 15:12:28 +0530

    Just a test commit to push new branch to heroku

--- ACS/settings.py
+++ ACS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lnwa=g3cx$wokp^n8k94*xsio+!7nqpyw-%mzq^=95y!#mja#h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
